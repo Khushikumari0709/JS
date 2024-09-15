4fdc5304f14bde54617a8fbc21682c67eb62b12f
Khushi Kumari <smstokhushi123@gmail.com>
2024-09-15 16:17:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyZNCM18tKHNZ76-_4EC_asGW6e8qQ63kZLP5m7gFaVQrSmR5h1hV3HsLFAMCvuI07F/exec'
+    const form = document.forms['submit-to-google-sheet']
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+    })
